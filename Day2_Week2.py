91e745084e065c3b8bcf6ef4dee07e9d0d01bdde
ArthurKingz <95949380+ArthurKingz@users.noreply.github.com>
2025-07-03 10:29:31 +0800

    Add files via upload
    1st Main upload of the Python Streamlit file.

---
+++ Day2_Week2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.image('./dell-logo-grill.jpg')
+st.title("Week 2 Python Web")
+
+
+st.date_input("Developed Date")
+st.radio("Select Department :",['SW&T','NPI','QA','Ops'])
+
+# st.camera_input("Case Reported")
